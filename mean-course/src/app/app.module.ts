82324fc3b19484659db0300f43963bb528cf2d2d
Eric Ross <eric.ross@hp.com>
2019-08-10 13:39:33 -0700

    2-way binding

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component'
@@ -7,10 +8,11 @@ import { PostCreateComponent } from './posts/post-create/post-create.component'
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
