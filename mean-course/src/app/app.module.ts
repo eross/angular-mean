28bdaeba32789450af2daf37534299ff17515479
Eric Ross <eric.ross@hp.com>
2019-08-11 14:09:54 -0700

    Added post list

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -5,21 +5,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import {
   MatInputModule,
   MatCardModule,
-  MatCard,
   MatButtonModule,
-  MatToolbarModule
+  MatToolbarModule,
+  MatExpansionModule
 } from '@angular/material'
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
-    HeaderComponent
+    HeaderComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
@@ -28,7 +30,8 @@ import { HeaderComponent } from './header/header.component';
     MatInputModule,
     MatCardModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
