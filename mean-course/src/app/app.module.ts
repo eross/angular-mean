787efcc83a94ef076352b3d6b2792b9c9a06e0f3
Eric Ross <eric.ross@hp.com>
2019-08-11 13:35:20 -0700

    20 -- adding toolbar

--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -2,16 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-import { MatInputModule, MatCardModule, MatCard, MatButtonModule } from '@angular/material'
+import {
+  MatInputModule,
+  MatCardModule,
+  MatCard,
+  MatButtonModule,
+  MatToolbarModule
+} from '@angular/material'
 
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
+import { HeaderComponent } from './header/header.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -19,7 +27,8 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
     BrowserAnimationsModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
